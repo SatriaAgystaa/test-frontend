f75033d90d91fb9febf6571db8d58e17e85a08ae
SatriaAgystaa <satriaagyy@gmail.com>
2025-07-22 19:06:48 +0700

    push

--- components/Testimonials/TestimonialCard.vue
+++ components/Testimonials/TestimonialCard.vue
@@ -15,7 +15,7 @@
     </div>
 
     <!-- Text Section -->
-    <div class="relative aspect-[3/4] bg-red-700 p-8 text-white flex flex-col justify-between">
+    <div class="relative aspect-[3/4] bg-[#A10501] p-8 text-white flex flex-col justify-between">
       <img
         src="/icons/baseicons/koma.svg"
         alt="Quote icon"
